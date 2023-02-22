86448c48a26ca600bc6e54ace26deeea2f79f932
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-22 12:04:32 +0530

    Product Routes and Controller Created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,59 @@
-exports.getAllproducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+const Product = require("../models/productModel");
+
+// Create Product --- Admin
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json(product);
+};
+
+// Get All Products
+exports.getAllproducts = async (req, res) => {
+  const product = await Product.find();
+  res.status(201).json(product);
+};
+
+// Update product --- Admin
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      message: "Product Not Found",
+    });
+  }
+
+  updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedProduct);
+};
+
+// Delete Product --- Admin
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      message: "Product Not Found",
+    });
+  }
+
+  await product.remove();
+
+  res.status(200).json({ message: "Product deleted succesfully" });
+};
+
+// Get Single Product
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      message: "Product Not Found",
+    });
+  }
+
+  res.status(200).json(product);
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const { getAllproducts } = require("../controllers/productController");
+const {
+  getAllproducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
+// Get All Products
 router.route("/products").get(getAllproducts);
+// Create a Product
+router.route("/product/new").post(createProduct);
+// Update a Product
+router.route("/product/:id").put(updateProduct);
+// Delete a Product
+router.route("/product/:id").delete(deleteProduct);
+// Get a single Product
+router.route("/product/:id").get(getProductDetails);
 
 module.exports = router;
