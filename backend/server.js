9a8b1333c0bd8b55136136e365e7f8c6f3f08856
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-22 10:32:05 +0530

    Initial Commit, Server Started

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
